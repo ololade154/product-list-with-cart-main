8e78b57f45c2ce22c5dbd368d3506878757953a3
Ololade154 <shogbaikeoluwakemi2020@gmail.com>
2026-02-17 18:35:56 +0100

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,12 @@
 import { Cart } from './Cart';
 import { Products } from './Products';
 
-// import data from './data';
 export const Home = () => {
   return (
     <>
-      {/* <div className="grid grid-cols-1 md:grid-cols-3 gap-2">
-        {data.map((item) => (
-          <div key={item.id}>
-            <img src={item.img.mobile} className="block md:hidden w-full" />
-
-            <img src={item.img.desktop} className="hidden md:block w-full" />
-          </div>
-        ))}
-      </div> */}
-      <div className="flex flex-col md:flex-row gap-2">
-        <Products className="flex-1 bg-yellow-500 border-2 border-b-emerald-700" />
-        <Cart className="bg-blue-700 border-2 border-red-500 w-full md:w-80 md:h-50 " />
+      <div className="flex flex-col md:flex-row gap-6 md:gap-4 md:p-8 py-4 px-6 md:w-full">
+        <Products className="flex-1 md:border-2 md:border-b-emerald-700" />
+        <Cart className="bg-blue-700 border-2 border-red-500 md:w-1/4 md:h-50 " />
       </div>
     </>
   );
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,13 +1,52 @@
+import { useState } from 'react';
 import data from './data';
 export const Products = ({ className }) => {
+  const [selected, setSelected] = useState({});
+
+  const toggleSelected = (id) => {
+    setSelected((prev) => ({ ...prev, [id]: !prev[id] }));
+  };
   return (
     <div className={className}>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-2">
+      <h1>Desserts</h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-4">
         {data.map((item) => (
-          <div key={item.id}>
-            <img src={item.img.mobile} className="block md:hidden w-full" />
-
-            <img src={item.img.desktop} className="hidden md:block w-full" />
+          <div key={item.id} className="bg-white shadow-md rounded-lg ">
+            <div className="relative w-full">
+              <img
+                src={item.img.mobile}
+                className="block md:hidden w-full object-cover rounded-lg "
+              />
+              <img
+                src={item.img.desktop}
+                className="hidden md:block w-full object-cover rounded-lg"
+              />
+              <div className="absolute -bottom-4 left-1/2 -translate-x-1/2 z-10 ">
+                {!selected[item.id] ? (
+                  <button
+                    onClick={() => toggleSelected(item.id)}
+                    className="cursor-pointer w-full bg-white border-color py-1.5 px-8 rounded-full "
+                  >
+                    <div className="flex gap-8">
+                      <div>P</div>
+                      <div>J</div>
+                    </div>
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => toggleSelected(item.id)}
+                    className="cursor-pointer w-full button py-1.5 px-8 rounded-full"
+                  >
+                    Hello
+                  </button>
+                )}
+              </div>
+            </div>
+            <div className="p-2">
+              <p>{item.category}</p>
+              <h1>{item.name}</h1>
+              <h2>${item.price}</h2>
+            </div>
           </div>
         ))}
       </div>
